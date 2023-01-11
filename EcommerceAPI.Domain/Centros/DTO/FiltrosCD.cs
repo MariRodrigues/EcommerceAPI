272dcd6c57bee12c6a172afeaa9dbeefda2999b9
Mariana Rodrigues <46384658+MariRodrigues@users.noreply.github.com>
2023-01-11 13:21:36 -0300

    cria endpoints de get na controller v2 de cd (#3)

--- EcommerceAPI.Domain/Centros/DTO/FiltrosCD.cs
+++ EcommerceAPI.Domain/Centros/DTO/FiltrosCD.cs
@@ -2,6 +2,7 @@
 {
     public class FiltrosCD
     {
+        public int? Id { get; set; }
         public string Nome { get; set; }
         public string Logradouro { get; set; }
         public int? Numero { get; set; }
