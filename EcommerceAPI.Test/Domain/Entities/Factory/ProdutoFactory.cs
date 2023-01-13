18e82e4a8df0c68f6c426325bb1a1f4a559c5a1d
Mariana Souza <Mari_rodrigue@hotmail.com>
2023-01-13 17:56:18 -0300

    Ajuste no Produto Factory

--- EcommerceAPI.Test/Domain/Entities/Factory/ProdutoFactory.cs
+++ EcommerceAPI.Test/Domain/Entities/Factory/ProdutoFactory.cs
@@ -2,6 +2,8 @@
 using EcommerceAPI.Domain.Categorias;
 using EcommerceAPI.Domain.Produtos;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace EcommerceAPI.Test.Domain.Entities.Factory
 {
@@ -30,20 +32,7 @@ namespace EcommerceAPI.Test.Domain.Entities.Factory
 
         public static Produto Create()
         {
-            var produto = new Faker<Produto>("pt_BR")
-                .RuleFor(x => x.Nome, (f, u) => f.Name.LastName())
-                .RuleFor(x => x.Id, (f, u) => f.IndexFaker)
-                .RuleFor(x => x.Peso, f => f.Random.Number(1, 10))
-                .RuleFor(x => x.Altura, f => f.Random.Number(1, 10))
-                .RuleFor(x => x.QuantidadeEstoque, f => f.Random.Number(1, 100))
-                .RuleFor(x => x.CentroDistribuicaoId, f => f.IndexFaker)
-                .RuleFor(x => x.SubcategoriaId, f => f.IndexFaker)
-                .RuleFor(x => x.CategoriaId, f => f.IndexFaker)
-                .RuleFor(x => x.Comprimento, f => f.Random.Number(1, 10))
-                .RuleFor(x => x.Url, f => f.Internet.Avatar())
-                .RuleFor(x => x.Valor, f => f.Random.Number(1, 10))
-                .RuleFor(x => x.Descricao, f => f.Lorem.Sentence());
-
+            var produto = Create(1).FirstOrDefault();
             return produto;
         }
 
