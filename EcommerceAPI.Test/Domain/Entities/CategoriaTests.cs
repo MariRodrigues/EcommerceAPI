1cabcef62d10c9c8c287e02997f7528856ae32d0
Mariana Souza <Mari_rodrigue@hotmail.com>
2023-01-27 15:15:05 -0300

    removendo teste

--- EcommerceAPI.Test/Domain/Entities/CategoriaTests.cs
+++ EcommerceAPI.Test/Domain/Entities/CategoriaTests.cs
@@ -25,15 +25,5 @@ namespace EcommerceAPI.Test.Domain.Entities
             Assert.True(categoria.Status);
         }
 
-        [Fact(DisplayName = "Status da categoria criada deve ser ativa")]
-        public void Deve_Ser_Criada_Categoria_Ativa_fail()
-        {
-            // Act
-            var categoria = CategoriaFactory.Create();
-
-            // Assert
-            Assert.False(categoria.Status);
-        }
-
     }
 }
