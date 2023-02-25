a3635a57462542d94a1558b6f6236a33e5dbeeb7
Mariana Souza <Mari_rodrigue@hotmail.com>
2023-02-25 11:56:30 -0300

    atualiza string de conexao

---
+++ EcommerceAPI.Test/Infra/Shared/UowTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Test.Infra.Shared
+{
+    public class UowTest : IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
